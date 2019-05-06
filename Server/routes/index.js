207b9927d68ab180dcd827408c52c88354590c43
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-05-06 20:48:41 +0530

    Working Sign-In/Register routes

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,66 +1,70 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const adminRouter = express.Router();
-
+const config = require("../config");
 const mongoose = require("mongoose");
 const User = mongoose.model("user");
+const saltrounds = 10;
 
 // Registers User to DataBase
 adminRouter.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
 
   User.findOne({ email }, (err, obj) => {
-    console.log("TCL: obj", obj);
     if (err) return res.status(200).json({ err });
 
     // Already a user with this email
-    if (obj) return res.status(200).json({ message: "User already exists" });
+    if (obj) return res.status(200).json({ message: "User already exists" }); //exit the loop if user already exists. It is saving the existing user again
     if (password != password2) {
-      return res.send("Password Inocorrect");
+      return res.status(200).json({ message: "Password doesn't Match" });
     }
-
     //Hashing Password
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) return res.status(500).json({ err });
-        let hashedPassword = hash;
+    bcrypt.hash(password, saltrounds, (err, hash) => {
+      const hashedPassword = hash;
 
-        var myData = new User({
-          name,
-          email,
-          password: hashedPassword
-        });
-        myData.save((err, savedUser) => {
-          if (err) return res.status(500).json({ err });
-          else res.status(200).json({ success: true });
-        });
+      var myData = new User({
+        name,
+        email,
+        password: hashedPassword
+      });
+      myData.save((err, savedUser) => {
+        if (err) return res.status(500).json({ err });
+        else {
+          res.status(200).json({ success: true });
+          console.log(myData);
+        }
       });
     });
   });
-
-  // res.redirect("/signin");
 });
 
+// res.redirect("/signin");
 //Log-in User
-adminRouter.get("/signin", (req, res) => {
-  User.findOne({ email: email }, function(err, obj) {
-    {
-      if (obj == undefined)
-        return res.status(404).json({ email: "User not found" });
-    }
-    //comparing Hashed Passwords
-    bcrypt.compare(password, User.password).then(isMatch => {
-      if (isMatch) {
-        const payload = { id: User.id, name: User.name };
-        jwt.sign(payload, config.secret, (err, token) => {
-          res.json({ token });
-        });
-      } else return res.status(400).json({ message: "Password Incorrect" });
-    });
+adminRouter.post("/signin", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, obj) => {
+    if (err) return res.status(500).json({ err });
+    if (!obj) return res.status(404).json({ message: "User not found" });
+
+    console.log(obj.password);
+    bcrypt
+      .compare(req.body.password, obj.password)
+      .then(isMatch => {
+        if (isMatch) {
+          const payload = { id: this.id, name: this.name };
+          jwt.sign(payload, config.secret, (err, token) => {
+            res.json({ auth: true, token: token });
+          });
+        }
+        else res.json({message:"User not found"})
+      })
+      .catch((err)=>{
+        res.status(400).json({ token: null, auth: false, err});
+      });
   });
-  // return res.redirect('/review')
 });
+
+// return res.redirect('/review')
 
 adminRouter.post("/submitReview", (req, res) => {
   const { movie, review, rating } = req.body;
